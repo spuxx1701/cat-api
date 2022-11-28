62eaa0738f5adb53413e9b6740d5c554f0d5a1b9
LeopoldHock <leopold.hock@deutschebahn.com>
2022-11-28 13:42:15 +0100

    added local mariadb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
